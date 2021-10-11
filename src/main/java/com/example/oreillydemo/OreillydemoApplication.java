6c64bc17947dd1651f3282710f4429a2f746f05b
mohamed <moh4med.ahmed@gmail.com>
2021-10-11 16:16:42 +0200

    Initial commit

---
+++ src/main/java/com/example/oreillydemo/OreillydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oreillydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OreillydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OreillydemoApplication.class, args);
+	}
+
+}
